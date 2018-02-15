df59c0857a543b150b284d7dbf1e0caad824bf2c
nmassey001 <neil.massey@stfc.ac.uk>
2018-02-15 14:13:03 +0000

    Added uploading of credentials and external storage type in JSON

--- jdma_client/jdma.py
+++ jdma_client/jdma.py
@@ -18,13 +18,13 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 class settings:
     """Settings for the xfc command line tool."""
 #    JDMA_SERVER_URL = "http://0.0.0.0:8001/jdma_control"  # location of the jdma_control server / app
-#    JDMA_SERVER_URL = "https://192.168.51.26/jdma_control"
-    JDMA_SERVER_URL = "http://192.168.51.26/jdma_control"
+    JDMA_SERVER_URL = "http://192.168.51.26/jdma_control"  # location of the test server / vagrant version
     JDMA_API_URL = JDMA_SERVER_URL + "/api/v1/"
 #    USER = os.environ["USER"] # the USER name
     USER = "nrmassey"
     VERSION = "0.1" # version of this software
     VERIFY = False
+    user_credentials = {}
 
 
 unit_list = zip(['bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB'], [0, 0, 1, 1, 1, 1, 1])
@@ -88,8 +88,8 @@ def do_help(args):
         print(bcolors.YELLOW + "help: supply command to seek help on." + bcolors.ENDC)
 
 def do_init(args):
-    """init <email address> : Initialize the JASMIN Data Migration App for your JASMIN login."""
-    ### Send the HTTP request (POST) to initialize a user.###
+    """init <email address> : Initialise the JASMIN Data Migration App for your JASMIN login."""
+    ### Send the HTTP request (POST) to initialise a user.###
     # get the email from the args
     email = args.arg
     if args.email:
@@ -111,15 +111,15 @@ def do_init(args):
         error = response.json()['error']
         user = response.json()['name']
         sys.stdout.write(bcolors.RED+\
-              "** ERROR ** - cannot initialize user " + user + " : " + error + bcolors.ENDC + "\n")
+              "** ERROR ** - cannot initialise user " + user + " : " + error + bcolors.ENDC + "\n")
     else:
         sys.stdout.write(bcolors.RED+\
-              "** ERROR ** - cannot initialize user " + user + " : 500 " + bcolors.ENDC + "\n")
+              "** ERROR ** - cannot initialise user " + user + " : 500 " + bcolors.ENDC + "\n")
         print_response_error(response)
 
 
 def do_email(args):
-    """email <email address> : Set / update user's email address for notifications."""
+    """email <email address> : Set / update your email address for notifications."""
     ###Send the HTTP request (PUT) to update the email address of the user by sending a PUT request.###
     # get the email from the args
     email = args.arg
@@ -144,7 +144,7 @@ def do_email(args):
 
 
 def do_info(args):
-    """info : get information about the user, including email address and notification setting."""
+    """info : get information about you, including email address and notification setting."""
     ###Send the HTTP request (GET) to return information about the user###
     url = settings.JDMA_API_URL + "user?name=" + settings.USER
     data = {"name" : settings.USER}
@@ -293,10 +293,10 @@ def do_list_requests():
         else:
             # print the header
             sys.stdout.write(bcolors.MAGENTA)
-            print "{:>11} {:<6} {:<8} {:<24} {:16} {:<11} {:<16}".format("request id", "type", "batch id", "workspace", "batch label", "stage", "date")
+            print "{:>6} {:<8} {:<8} {:<16} {:16} {:<11} {:<16}".format("req id", "type", "batch id", "workspace", "batch label", "stage", "date")
             sys.stdout.write(bcolors.ENDC)
             for r in data["requests"]:
-                print "{:>11} {:<6} {:<8} {:<24} {:16} {:<11} {:<16}".format(\
+                print "{:>6} {:<8} {:<8} {:<16} {:16} {:<11} {:<16}".format(\
                     r["request_id"],
                     get_request_type(r["request_type"]),
                     r["migration_id"],
@@ -318,11 +318,14 @@ def do_batch(args):
     """batch <batch_id> : List all batches, or all the details of a particular batch with <batch_id>."""
     ### Send the HTTP request (GET) to get the details of a single migration for the user.
     ### Optionally filter on the workspace.
-    # get the workspace from the arguments
-    if args.workspace:
-        workspace = args.workspace
+    # get the workspace from the arguments, or from the config file
+    if args.workspace == "default":
+        try:
+            workspace = settings.user_credentials["default_gws"]
+        except:
+            workspace = None
     else:
-        workspace = None
+        workspace = args.workspace
     # determine whether we should list one batch or many
     if len(args.arg):
         batch_id = int(args.arg)
@@ -350,6 +353,8 @@ def do_batch(args):
         if "registered_date" in data:
             sys.stdout.write("    Date         : " + data["registered_date"][0:16].replace("T"," ")+"\n")
         sys.stdout.write("    Stage        : " + get_request_stage(data["stage"])+"\n")
+        if "storage" in data:
+            sys.stdout.write("    Ex. storage  : " + str(data["storage"])+"\n")
         if "external_id" in data:
             sys.stdout.write("    External id  : " + str(data["external_id"])+"\n")
         sys.stdout.write("    Directory    : " + data["original_path"]+"\n")
@@ -393,13 +398,14 @@ def do_list_batches(workspace=None):
         else:
             # print the header
             sys.stdout.write(bcolors.MAGENTA)
-            print "{:>8} {:<24} {:16} {:<11} {:<16} {:<16}".format("batch id", "workspace", "batch label", "stage", "date", "permission")
+            print "{:>8} {:<16} {:<16} {:<16} {:<11} {:<16}".format("batch id", "workspace", "batch label", "storage", "stage", "date")
             sys.stdout.write(bcolors.ENDC)
             for r in data["migrations"]:
-                print "{:>8} {:<24} {:<16} {:11} {:<16}".format(\
+                print "{:>8} {:<16} {:<16} {:<16} {:<11} {:<16}".format(\
                     r["migration_id"],
                     r["workspace"][0:16],
                     r["label"][0:16],
+                    r["storage"][0:16],
                     get_request_stage(r["stage"]),
                     r["registered_date"][0:16].replace("T"," "))
     elif response.status_code < 500:
@@ -412,35 +418,59 @@ def do_list_batches(workspace=None):
         print_response_error(response)
 
 
+def add_credentials(args, data):
+    if args.storage:
+        # check whether there is a default storage backend and read it from the credentials file if there is
+        if args.storage == "default":
+            try:
+                storage = settings.user_credentials['default_storage']
+            except:
+                # the default default is elastictape
+                storage = "elastictape"
+        else:
+            storage = args.storage
+        # add to JSON data
+        data["storage"] = storage
+        # read the credentials for this backend from the ~/.jdma.json file and add them to the request
+        # if there are no credentials in ~/.jdma.json for this backend then add nothing
+        # if the backend is expecting credentials and none are supplied then the server will return an error
+        try:
+            data["credentials"] = settings.user_credentials['storage'][storage]['required_credentials']
+        except:
+            # don't add any credentials
+            pass
+
+
 def do_migrate_or_put(args, request_type):
     ###Send the HTTP request (POST) to indicate a directory is to be migrated.###
     # get the path, workspace and label (if any) from the args
     assert(request_type == "PUT" or request_type == "MIGRATE")
-    if len(args.arg):
-        path = args.arg
-    else:
-        path = os.getcwd()
-    if args.workspace:
-        workspace = args.workspace
-    else:
-        workspace = None
-    if args.label:
-        label = args.label
-    else:
-        label = None
-
     url = settings.JDMA_API_URL + "request"
     # set the user and request type data
     data = {"name" : settings.USER,
             "request_type" : request_type}
+
     # add the path and workspace - if the workspace is none then don't add
     # in this case the HTTP API will return an error as a workspace is required
-    if path:
-        data["original_path"] = path
-    if workspace:
-        data["workspace"] = workspace
-    if label:
+    if len(args.arg):
+        data["original_path"] = args.arg
+    else:
+        data["original_path"] = os.getcwd()
+
+    if args.workspace == "default":
+        try:
+            workspace = settings.user_credentials["default_gws"]
+        except:
+            workspace = None
+    else:
+        workspace = args.workspace
+    data["workspace"] = workspace
+
+    if args.label:
         data["label"] = label
+
+    # add the credentials to the request
+    add_credentials(args, data)
 
     # do the request (POST)
     response = requests.post(url, data=json.dumps(data), verify=settings.VERIFY)
@@ -453,6 +483,7 @@ def do_migrate_or_put(args, request_type):
         sys.stdout.write("    Label        : " + data["label"]+"\n")
         sys.stdout.write("    Date         : " + data["registered_date"][0:16].replace("T"," ")+"\n")
         sys.stdout.write("    Request type : " + get_request_type(data["request_type"])+"\n")
+        sys.stdout.write("    Ex. storage  : " + data["storage"]+"\n")
         sys.stdout.write("    Stage        : " + get_request_stage(data["stage"])+"\n")
         sys.stdout.write("    Directory    : " + data["original_path"]+"\n")
         sys.stdout.write("    Unix uid     : " + data["unix_user_id"]+"\n")
@@ -462,12 +493,12 @@ def do_migrate_or_put(args, request_type):
         # print the error
         error_data = response.json()
         error_msg = "** ERROR ** - cannot "+request_type+" directory"
-        if path:
-            error_msg += " " + path
+        if "original_path" in error_data:
+            error_msg += " " + error_data["original_path"]
         error_msg += " for user " + settings.USER
-        if workspace:
-            error_msg += " in workspace " + workspace
-        if error_data["error"]:
+        if "workspace" in error_data:
+            error_msg += " in workspace " + error_data["workspace"]
+        if "error" in error_data:
             error_msg += " : " + error_data["error"]
         error_msg += "\n"
         sys.stdout.write(bcolors.RED + error_msg)
@@ -477,12 +508,12 @@ def do_migrate_or_put(args, request_type):
 
 
 def do_put(args):
-    """put <path>|<filelist>: Create a batch upload of the current directory, or directory in <path> or a list of files (with --filelist switch).  Use --label= to give the batch a label."""
+    """put <path>|<filelist>: Create a batch upload of the current directory, or directory in <path> or a list of files (with --filelist switch).  Use --label= to give the batch a label.  Use --storage to specify which external storage to target for the migration.  Use command list_storage to list all the available storage targets."""
     do_migrate_or_put(args, "PUT")
 
 
 def do_migrate(args):
-    """migrate <path>|<filelist>: Create a batch upload of the current directory, or directory in <path> or a list of files (with --filelist switch).  Use --label= to give the batch a label.  The data in the directory or filelist will be deleted after the upload is completed."""
+    """migrate <path>|<filelist>: Create a batch upload of the current directory, or directory in <path> or a list of files (with --filelist switch).  Use --label= to give the batch a label.  Use --storage to specify which external storage to target for the migration.  Use command list_storage to list all the available storage targets.  The data in the directory or filelist will be deleted after the upload is completed."""
     do_migrate_or_put(args, "MIGRATE")
 
 
@@ -511,6 +542,9 @@ def do_get(args):
         data["migration_id"] = batch_id
     if target_dir:
         data["target_path"] = target_dir
+
+    # add the credentials to the request
+    add_credentials(args, data)
 
     # do the request (POST)
     response = requests.post(url, data=json.dumps(data), verify=settings.VERIFY)
@@ -544,6 +578,54 @@ def do_get(args):
 def do_get_files(args):
     """get_files <batch_id> <filelist> : Retrieve a (subset) list of files from a batch with <batch_id>.  <filelist> is the name of a file containing a list of filenames to retrieve.  The filenames in the filelist must be the full path, as obtained by list <batch_id>."""
     raise NotImplementedError
+
+
+def do_delete(args):
+    """delete <batch_id> : Delete the batch with <batch_id> from the storage"""
+    raise NotImplementedError
+
+
+def list_storage():
+    """Return a list of the available external storage backends"""
+    url = settings.JDMA_API_URL + "list_backends"
+    response = requests.get(url, verify=settings.VERIFY)
+    storage = {}
+    if response.status_code == 200:
+        # parse the JSON that comes back
+        data = response.json()
+        for r in range(0, len(data)):
+            storage[data.keys()[r]] = data[data.keys()[r]]
+
+    elif response.status_code < 500:
+        error_msg = "** ERROR ** - cannot list storage"
+        try:
+            error_data = response.json()
+            error_msg += ": " + str(error_data["error"])
+        except:
+            error_msg += " (" + str(response.status_code) + ")"
+        error_msg += "\n"
+        sys.stdout.write(bcolors.RED + error_msg)
+        sys.stdout.write(bcolors.ENDC)
+    else:
+        print response.status_code
+        print_response_error(response)
+    return storage
+
+
+def do_list_storage(args):
+    """list_storage : list the storage targets that batches can be written to"""
+    storage = list_storage()
+    if storage != {}:
+        sys.stdout.write(bcolors.MAGENTA)
+        print "{:>4} {:<24} {:16}".format("", "Name", "Short ID")
+        sys.stdout.write(bcolors.ENDC)
+        r = 0
+        for k in storage:
+            sys.stdout.write("{:>4} {:<24} {:16}".format(r, storage[k], k))
+            sys.stdout.write("\n")
+            r+=1
+        sys.stdout.write(bcolors.ENDC)
+
 
 def do_list(args):
     """list <batch_id> : List the original paths of files in a batch."""
@@ -606,10 +688,34 @@ def do_label(args):
         print_response_error(response)
 
 
+def read_credentials_file():
+    """Read in the credentials file.  It is JSON formatted"""
+    # path is in user directory
+    user_home = os.environ["HOME"]
+
+    # add the config file name
+    jdma_user_config_filename = user_home + "/" + ".jdma.json"
+
+    # open the file
+    try:
+        fp = open(jdma_user_config_filename)
+        # deserialize from the JSON
+        jdma_user_credentials = json.load(fp)
+        # close the config file
+        fp.close()
+    except IOError:
+        raise IOError("User credentials file does not exist with path: " + jdma_user_config_filename)
+    except Exception as e:
+        raise IOError("Error in credentials file at: " + jdma_user_config_filename + "\n" + str(e))
+
+    return jdma_user_credentials
+
+
 def main():
     command_help = "Type help <command> to get help on a specific command"
-    command_choices = ["init", "email", "info", "notify", "request", "batch", "put",
-                       "get", "list", "find", "label", "migrate", "get_files", "help"]
+    command_choices = ["init", "email", "info", "notify", "request", "batch",
+                       "put", "get", "list", "find", "label", "migrate", "get_files", "delete",
+                       "list_storage", "help"]
     command_text = "[" + " | ".join(command_choices) + "]"
 
     parser = argparse.ArgumentParser(prog="JDMA", formatter_class=argparse.RawTextHelpFormatter,
@@ -618,14 +724,19 @@ def main():
     parser.add_argument("cmd", choices=command_choices, metavar="command", help=command_text)
     parser.add_argument("arg", help="Argument to the command", default="", nargs="?")
     parser.add_argument("-e", "--email", action="store", default="", help="Email address for user in the init and email commands.")
-    parser.add_argument("-w", "--workspace", action="store", default="", help="Group workspace to use in the request.")
+    parser.add_argument("-w", "--workspace", action="store", default="default", help="Group workspace to use in the request.")
     parser.add_argument("-l", "--label", action="store", default="", help="Label to name the request.")
     parser.add_argument("-r", "--target", action="store", default="", help="Optional target directory for GET.")
     parser.add_argument("-f", "--filelist", action="store_true", default=False, help="Use filelist in put and migrate.")
+    parser.add_argument("-s", "--storage", action="store", default="default", help="Specify external storage to use for migration.  Use "+bcolors.BOLD+"list_migration"+bcolors.ENDC+" to list the available storage targets.  Default is elastictape.")
+
+    # read the credentials file
+    settings.user_credentials = read_credentials_file()
 
     args = parser.parse_args()
 
     method = globals().get("do_" + args.cmd)
+
     method(args)
 
 if __name__ == "__main__":
