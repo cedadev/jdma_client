fb6db56b2494fc18b2982bb66caae09d7af09a47
nmassey001 <neil.massey@stfc.ac.uk>
2019-06-17 16:31:43 +0100

    Changed JDMA server back to jdma-tes

--- jdma_client/jdma_common.py
+++ jdma_client/jdma_common.py
@@ -11,9 +11,9 @@ import requests
 class settings:
     """Settings for the jdma command line tool."""
     # location of the jdma_control server / app
-    #JDMA_SERVER_URL = "https://jdma-test.ceda.ac.uk/jdma_control"
+    JDMA_SERVER_URL = "https://jdma-test.ceda.ac.uk/jdma_control"
     # !!! Test server - don't commit with this line !!!
-    JDMA_SERVER_URL = "https://192.168.51.26/jdma_control"
+    #JDMA_SERVER_URL = "https://192.168.51.26/jdma_control"
     JDMA_API_URL = JDMA_SERVER_URL + "/api/v1/"
     # template for the .config file
     JDMA_CONFIG_URL = "https://raw.githubusercontent.com/cedadev/jdma_client/master/jdma_client/.jdma.json.template"
