dc6f547d634736f01a8a2229b17a9134b240a494
nmassey001 <neil.massey@stfc.ac.uk>
2019-09-18 08:35:51 +0100

    Upped version number to 0.2.18

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='jdma_client',
-    version='0.2.15',
+    version='0.2.18',
     packages=['jdma_client'],
     install_requires=['requests',
                       'python_dateutil',
